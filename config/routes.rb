a01be855291540e35c1fb961c9040deee146ef51
Jacob Smith <jsmith2021@brandeis.edu>
2021-03-21 22:12:36 -0400

    added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :courses
   resources :subjects
   get 'static_pages/subjects'
@@ -6,5 +7,7 @@ Rails.application.routes.draw do
   get 'static_pages/instructors'
   resources :instructors
   root 'welcome#index'
+  get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
